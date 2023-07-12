d60b07db3dc858cd0c4a06fc2dc7e45f79b663f1
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-12 17:04:13 +0900

    Controller 테스트

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableJpaAuditing
+
 @EnableScheduling
 @SpringBootApplication
+//@EnableJpaAuditing
 public class MyselectshopApplication {
 
     public static void main(String[] args) {
